e4dcb135d0af54b8a51e7c458e392edc0f6715a8
Brent Chow <brentchow@gmail.com>
2017-05-15 15:59:13 -0400

    Added Altspace SDK depenedencies & basic cube example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as _THREE from 'three';
+declare const THREE: typeof _THREE;
+
+declare const require: any;
+const altspace = require('exports-loader?altspace!altspace');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  private _scene: THREE.Scene = new THREE.Scene();
+
+  private _geometry: THREE.BoxGeometry = new THREE.BoxGeometry(200, 200, 200);
+  private _mesh: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+  private _cube: THREE.Mesh = new THREE.Mesh(this._geometry, this._mesh);
+
+  private _renderer = altspace.getThreeJSRenderer({version: '0.2.0'});
+
+  private _render() {
+    requestAnimationFrame(() => this._render());
+
+    this._cube.rotation.x += 0.01;
+    this._cube.rotation.y += 0.01;
+
+    this._renderer.render(this._scene);
+  }
+
+  ngOnInit() {
+    this._scene.add(this._cube);
+
+    this._render();
+  }
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -28,9 +28,9 @@
 // import 'core-js/es6/math';
 // import 'core-js/es6/string';
 // import 'core-js/es6/date';
-// import 'core-js/es6/array';
+import 'core-js/es6/array';
 // import 'core-js/es6/regexp';
-// import 'core-js/es6/map';
+import 'core-js/es6/map';
 // import 'core-js/es6/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
